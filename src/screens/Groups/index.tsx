959aba0b24971b7b89ea101a082a71536d2b59e0
Arthur Moreira <arthurmcs96@gmail.com>
2023-04-25 16:33:52 -0300

    create GroupCard component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,14 @@
 import Header from '@components/Header';
 import { Container } from './style';
 import HighLight from '@components/Highlight';
+import GroupCard from '@components/GroupCard';
 
 export function Groups() {
     return (
         <Container>
             <Header />
             <HighLight title='Turmas' subtitle='jogue com a sua turma' />
+            <GroupCard title='ndfjnsvjnjdfn' />
         </Container>
     );
 }
