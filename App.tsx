f4edfc30c65c37ac677949a9f3521fdc9f99cca1
Arthur Moreira <arthurmcs96@gmail.com>
2023-04-27 18:12:14 -0300

    style the input placeholder

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import { StatusBar } from "react-native";
-import { Groups } from "@screens/Groups";
 import theme from "@theme/index";
 import { ThemeProvider } from "styled-components";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
