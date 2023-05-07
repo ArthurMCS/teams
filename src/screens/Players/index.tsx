010a86ea8ce460a6608f43ae8316c9bf21623458
Arthur Moreira <arthurmcs96@gmail.com>
2023-05-06 21:53:10 -0300

    finalize the player list

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -7,6 +7,8 @@ import Input from '@components/Input'
 import Filter from '@components/Filter'
 import { FlatList } from 'react-native'
 import PlayerCard from '@components/PlayerCard'
+import ListEmpty from '@components/ListEmpty'
+import Button from '@components/Button'
 
 export default function Players() {
   const [team, setTeam] = useState('Time A');
@@ -57,7 +59,21 @@ export default function Players() {
               onRemove={() => {}}
             />
           )}
-        />      
+          ListEmptyComponent={() => (
+            <ListEmpty 
+              message="Não há pessoas nesse time"
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={[
+            { paddingBottom: 100 },
+            players.length === 0 && { flex: 1 }
+          ]}
+        />
+        <Button
+          title="Remover item"
+          type="SECONDARY"
+        />    
     </Container>
   )
 }
